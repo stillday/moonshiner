d1aea32cd12fce7a5e7d7f97955316c4a09fd988
JD <jan-dennisd@outlook.at>
2024-12-17 21:38:18 +0100

    reworking app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import { Component } from "@wordpress/element";
+import { createElement, Component } from "@wordpress/element";
 import '/style.css';
-console.log('ich werde geladen');
 export default class App extends Component {
     render() {
-        return (
-            <div>
-                <h1>Wello, WordPress and React</h1>
-            </div>
+        return createElement (
+            'div',
+            null,
+            createElement('h1', null, 'Wello, WordPress and React')
         );
     }
 }
