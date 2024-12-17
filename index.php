6eea523756855f6ca45d4ac4b574520bf7dce2c8
JD <jan-dennisd@outlook.at>
2024-12-17 21:32:58 +0100

    fix small problem

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="<?php bloginfo('charset'); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Moonshiner React Dashboard</title>
-        <? php wp_head(); ?>
+        <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
         <div id="app"></div> <!-- React-container -->
