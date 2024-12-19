9c516d5013045882c972e175b73a93228179185c
JD <jan-dennisd@outlook.at>
2024-12-19 23:48:12 +0100

    remove misstakes, working on desing

--- src/components/DashboardWelcome.js
+++ src/components/DashboardWelcome.js
@@ -4,20 +4,25 @@ class DashboardWelcome extends Component {
     render() {
         const { darkTheme } = this.props;
 
-        const userName = window?.wpApiSettings?.userName || 'Guest';
+        const userName = window?.wpApiSettings?.user?.name || 'Guest';
         const userProfilImage = window?.wpApiSettings?.user?.avatar || 'https://www.gravatar.com/avatar/';
 
         const welcomeClass = darkTheme ? 'welcome dark' : 'welcome light';
 
         return createElement(
             'section',
-            { className: 'welcome-class' },
-            createElement('div', { className: 'user-profile' },
-                createElement('h2', null, 'Welcome, ${userName}'),
-                createElement('img', { src: userProfilImage, alt: '${userName} Profile Image' },
-                ),
-                createElement('p', null, 'This is the dashboard area for the Moonshiner theme.'),
+            { className: welcomeClass },
+            createElement(
+                'div',
+                { className: 'welcome-header' },
+                createElement('h1', { className: 'welcome-title' }, 'Welcome to the Moonshiner Theme'),
             ),
+            createElement('div', { className: 'welcome-user' },
+                createElement('h2', { className: 'welcome-message' } , 'Welcome, ', userName),
+                createElement('img', { src: userProfilImage, alt: `${userName} Profile Image`, className: 'welcome-avatar' },
+                ),
+            ),
+            createElement('p', { className: 'welcome-intro'}, 'This is the dashboard area for the Moonshiner theme.'),
         );
     }
 }
