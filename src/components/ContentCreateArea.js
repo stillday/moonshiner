435431828164b260c70d3386cd91eea3c8cc780a
JD <jan-dennisd@outlook.at>
2024-12-19 23:48:44 +0100

    include icons

--- src/components/ContentCreateArea.js
+++ src/components/ContentCreateArea.js
@@ -2,30 +2,30 @@ import { createElement, Component } from "@wordpress/element";
 
 const actions = {
     administrator: [
-        { id: "1", label: "Beitrag erstellen", url: "/wp-admin/post-new.php" },
-        { id: "2", label: "Seiten erstellen", url: "/wp-admin/post-new.php?post_type=page" },
-        { id: "3", label: "Medien hinzufügen", url: "/wp-admin/media-new.php" },
-        { id: "4", label: "Benutzer hinzufügen", url: "/wp-admin/user-new.php" },
-        { id: "5", label: "Kommentare", url: "/wp-admin/edit-comments.php" },
-        { id: "6", label: "Alle Beiträge", url: "/wp-admin/edit.php" },
-        { id: "7", label: "Themes", url: "/wp-admin/themes.php" },
-        { id: "8", label: "Anpassen", url: "/wp-admin/customize.php" },
-        { id: "9", label: "Plugins", url: "/wp-admin/plugins.php" },
-        { id: "10", label: "Einstellungen", url: "/wp-admin/options-general.php" },
+        { id: "1", label: "Beitrag erstellen", url: "/wp-admin/post-new.php", icon: "dashicons-edit" },
+        { id: "2", label: "Seiten erstellen", url: "/wp-admin/post-new.php?post_type=page", icon: "dashicons-admin-page" },
+        { id: "3", label: "Medien hinzufügen", url: "/wp-admin/media-new.php", icon: "dashicons-admin-media" },
+        { id: "4", label: "Benutzer hinzufügen", url: "/wp-admin/user-new.php", icon: "dashicons-admin-users" },
+        { id: "5", label: "Kommentare", url: "/wp-admin/edit-comments.php", icon: "dashicons-admin-comments" },
+        { id: "6", label: "Alle Beiträge", url: "/wp-admin/edit.php", icon: "dashicons-admin-post" },
+        { id: "7", label: "Themes", url: "/wp-admin/themes.php", icon: "dashicons-admin-appearance" },
+        { id: "8", label: "Anpassen", url: "/wp-admin/customize.php", icon: "dashicons-admin-customizer" },
+        { id: "9", label: "Plugins", url: "/wp-admin/plugins.php", icon: "dashicons-admin-plugins" },
+        { id: "10", label: "Einstellungen", url: "/wp-admin/options-general.php", icon: "dashicons-admin-settings" },
     ],
 
     editor: [
-        { id: "1", label: "Beitrag erstellen", url: "/wp-admin/post-new.php" },
-        { id: "2", label: "Seiten erstellen", url: "/wp-admin/post-new.php?post_type=page" },
-        { id: "3", label: "Medien hinzufügen", url: "/wp-admin/media-new.php" },
-        { id: "4", label: "Kommentare", url: "/wp-admin/edit-comments.php" },
-        { id: "5", label: "Alle Beiträge", url: "/wp-admin/edit.php" },
+        { id: "1", label: "Beitrag erstellen", url: "/wp-admin/post-new.php", icon: "dashicons-edit" },
+        { id: "2", label: "Seiten erstellen", url: "/wp-admin/post-new.php?post_type=page", icon: "dashicons-admin-page" },
+        { id: "3", label: "Medien hinzufügen", url: "/wp-admin/media-new.php", icon: "dashicons-admin-media" },
+        { id: "4", label: "Kommentare", url: "/wp-admin/edit-comments.php", icon: "dashicons-admin-comments" },
+        { id: "5", label: "Alle Beiträge", url: "/wp-admin/edit.php", icon: "dashicons-admin-post" },
     ],
 
     guest: [
-        { id: "1", label: "Anmelden", url: "/wp-login.php" },
-        { id: "2", label: "Registrieren", url: "/wp-login.php?action=register" },
-        { id: "3", label: "Passwort vergessen?", url: "/wp-login.php?action=lostpassword" },
+        { id: "1", label: "Anmelden", url: "/wp-login.php", icon: "dashicons-lock" },
+        { id: "2", label: "Registrieren", url: "/wp-login.php?action=register", icon: "dashicons-admin-users" },
+        { id: "3", label: "Passwort vergessen?", url: "/wp-login.php?action=lostpassword", icon: "dashicons-editor-help" },
     ],
 };
 
@@ -60,6 +60,10 @@ class ContentCreateArea extends Component {
                     createElement(
                         'li',
                         { className: 'content-item', key: action.id },
+                        createElement(
+                            'span',
+                            { className: `content-icon dashicons ${action.icon}` }
+                        ),
                         createElement(
                             'a',
                             { 
